90e0b7df9a38fb0e1fe16fba727bb260286ab11b
ritik-apurwa <ritikapurwa3500@gmail.com>
2024-07-07 15:59:57 +0530

    check

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="min-h-screen">
+      <body className={cn("min-h-screem")}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-  return (  
-    <div>page</div>
-  )
-}
+  return (
+    <div>
+      <h1 className="text-red-400">Home</h1>
+    </div>
+  );
+};
 
-export default page
+export default page;
